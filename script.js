daf5a5e6bd71e8fcc3f9dc49de9a2babc1b0b950
zzh0c <zzh0czz@gmail.com>
2020-03-30 00:16:34 +0300

    fix: slider animation

--- script.js
+++ script.js
@@ -3,35 +3,44 @@ let Arrow_Right = document.querySelector("#ArrowR");
 let Slide0 = document.querySelector(".Slide_0");
 let Slide1 = document.querySelector(".Slide_1");
 
-Arrow_Left.addEventListener("click", function () {
-    Slide0.classList.toggle("hidden");
-    Slide1.classList.toggle("hidden");
 
-
-});
-
-Arrow_Right.addEventListener("click", function () {
-
-if (Slide1.classList.contains("hidden")) {
-    Slide1.classList.toggle("hidden");
-    Slide1.classList.toggle("move_Left");
-    Slide0.classList.toggle("move_Right");
-    Slide0.addEventListener("transitionend", function () {
-        Slide0.classList.add("hidden");
-        Slide1.classList.remove("move_Left");
-        Slide0.classList.remove("move_Right");
+function ToRight(slideF, slideS) {
+    slideS.classList.toggle("hidden");
+    slideF.classList.toggle("move_Right");
+    slideF.addEventListener("transitionend", function () {
+        slideF.classList.add("hidden");
+        slideF.classList.remove("move_Right");
     });
 }
 
+Arrow_Right.addEventListener("click", function () {
+
+    if (Slide1.classList.contains("hidden")) {
+        ToRight(Slide0, Slide1);
+    }
+
     if (Slide0.classList.contains("hidden")) {
-        Slide0.classList.toggle("hidden");
+        ToRight(Slide1, Slide0);
+    }
+});
 
-        Slide1.classList.toggle("move_Right");
-        Slide1.addEventListener("transitionend", function () {
-            Slide1.classList.add("hidden");
-            Slide0.classList.remove("move_Left");
-            Slide1.classList.remove("move_Right");
-        });
 
+function ToLeft(slideF, slideS) {
+    slideS.classList.toggle("hidden");
+    slideF.classList.toggle("move_Left");
+    slideF.addEventListener("transitionend", function () {
+        slideF.classList.add("hidden");
+        slideF.classList.remove("move_Left");
+    });
+}
+
+Arrow_Left.addEventListener("click", function () {
+
+    if (Slide1.classList.contains("hidden")) {
+        ToLeft(Slide0, Slide1);
+    }
+
+    if (Slide0.classList.contains("hidden")) {
+        ToLeft(Slide1, Slide0);
     }
 });
