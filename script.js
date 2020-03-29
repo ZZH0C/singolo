65e1d58da2ac9469313b04bdca3a6a974ff69194
zzh0c <zzh0czz@gmail.com>
2020-03-29 23:36:19 +0300

    refactor: slider

--- script.js
+++ script.js
@@ -5,14 +5,33 @@ let Slide1 = document.querySelector(".Slide_1");
 
 Arrow_Left.addEventListener("click", function () {
     Slide0.classList.toggle("hidden");
-
     Slide1.classList.toggle("hidden");
+
+
 });
 
 Arrow_Right.addEventListener("click", function () {
 
-    Slide0.classList.toggle("hidden");
-
+if (Slide1.classList.contains("hidden")) {
     Slide1.classList.toggle("hidden");
+    Slide1.classList.toggle("move_Left");
+    Slide0.classList.toggle("move_Right");
+    Slide0.addEventListener("transitionend", function () {
+        Slide0.classList.add("hidden");
+        Slide1.classList.remove("move_Left");
+        Slide0.classList.remove("move_Right");
+    });
+}
 
+    if (Slide0.classList.contains("hidden")) {
+        Slide0.classList.toggle("hidden");
+
+        Slide1.classList.toggle("move_Right");
+        Slide1.addEventListener("transitionend", function () {
+            Slide1.classList.add("hidden");
+            Slide0.classList.remove("move_Left");
+            Slide1.classList.remove("move_Right");
+        });
+
+    }
 });
